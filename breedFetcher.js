038f0131ffac3820ac27f177184e13bdd19e3118
Yasin Hussein <bulaale116@gmail.com>
2023-12-12 10:42:05 -0700

    testing json cats

--- breedFetcher.js
+++ breedFetcher.js
@@ -3,18 +3,29 @@ const request = require('request');
 
 const cfaUrl = "http://cfa.org/Breeds/BreedsSthruT/Siberian.aspx";
 
-request(cfaUrl, (error, response, body) => {
-  if (error) {
-    console.error('Error:', error);
-    return;
-  }
+const fetchBreedDescription = function(breedName, callback) {
 
-  if (response.statusCode !== 200) {
-    console.error('Failed to fetch data. Status code:', response.statusCode);
-    return;
-  }
+};
+fetchBreedDescription('Siberian', (error, description) => {
+  request(cfaUrl, (error, response, body) => {
+    if (error) {
+      console.error('Error:', error);
+      return;
+    }
+  
+    if (response.statusCode !== 200) {
+      console.error('Failed to fetch data. Status code:', response.statusCode);
+      return;
+    }
+  
+    const data = JSON.parse(body);
+  
+    console.log('Fetched data:', data);
+  });
 
-  const data = JSON.parse(body);
-
-  console.log('Fetched data:', data);
 });
+
+
+module.exports = { fetchBreedDescription };
+
+
