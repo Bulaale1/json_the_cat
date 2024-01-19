9826818ce44aee6ff3755aa2f04be2f43b5c544f
Yasin Hussein <bulaale116@gmail.com>
2024-01-18 19:55:51 -0700

    refactored code of breedFetcher function

--- breedFetcher.js
+++ breedFetcher.js
@@ -2,22 +2,25 @@
 const request = require('request');
 
 const cfaUrl = 'https://api.thecatapi.com/v1/breeds/search?q=sib';
-request(cfaUrl, (error, response, body) => {
-  if (error) {
-    console.error('Error:', error);
-    return;
-  }
-  
-  if (response.statusCode !== 200) {
-    console.error('Failed to fetch data. Status code:', response.statusCode);
-    return;
-  }
-  
-  const data = JSON.parse(body);
-  console.log(typeof data);
-  const args = process.argv.slice(2);
-  console.log('Fetched data:', data[0]);
 
-});
+const fetchBreedDescription = function(breedName, callback) {
+  request(cfaUrl, (error, response, body) => {
+    if (error) {
+      console.error('Error:', error);
+      return;
+    }
+    
+    if (response.statusCode !== 200) {
+      console.error('Failed to fetch data. Status code:', response.statusCode);
+      return;
+    }
+    
+    const data = JSON.parse(body);
+    callback(data[0][breedName]);
+  });
+
+};
+
+module.exports = { fetchBreedDescription };
 
 
---
+++ index.js
@@ -0,0 +1,12 @@
+/* eslint-disable linebreak-style */
+const { fetchBreedDescription } = require('./breedFetcher');
+
+const breedName = process.argv[2];
+
+fetchBreedDescription(breedName, (error, desc) => {
+  if (error) {
+    console.log('Error fetch details:', error);
+  } else {
+    console.log(desc);
+  }
+});
